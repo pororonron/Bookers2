a5a5cf58f3928b922f86dc0d4dec1aa91d1e31d8
pororonron <153398697+pororonron@users.noreply.github.com>
2024-02-27 08:57:33 +0000

    [Add] Userの編集画面

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -28,6 +28,6 @@ class BooksController < ApplicationController
   private
   
   def book_params
-    params.require(:book).permit(:title, :image, :body)
+    params.require(:book).permit(:title, :body)
   end
 end
