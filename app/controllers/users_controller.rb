a457ba5db3527672daa6651d25da5888341ea779
pororonron <153398697+pororonron@users.noreply.github.com>
2024-02-28 16:07:33 +0000

    [Add] リンクの遷移先

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,13 @@
 class UsersController < ApplicationController
   def index
+    @user = current_user
+    @new_book = Book.new
     @users = User.all
   end
 
   def show
     @user = User.find(params[:id])
+    @new_book = Book.new
     @books = @user.books
   end
 
