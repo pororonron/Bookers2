7fe71ef57bc51655c719070d1d96170b2d832b25
pororonron <153398697+pororonron@users.noreply.github.com>
2024-03-01 16:31:22 +0000

    [Add] Topページのレイアウト

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_action :is_matching_login_user, only: [:edit, :update, :destroy]
+  before_action :is_matching_login_user, only: [:edit, :update]
   before_action :authenticate_user!, except: [:top, :about]
   before_action :configure_permitted_parameters, if: :devise_controller?
 
